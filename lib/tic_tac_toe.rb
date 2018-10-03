2bcdd91a1eb7bfaec162e9c33b3a8142afeb3f60
Anandha Sudhan <rasudhan@gmail.com>
2018-10-03 02:38:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,6 +16,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
